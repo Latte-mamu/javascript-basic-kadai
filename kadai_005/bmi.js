7e8a5fffdc64af0bc894ec9d9259318758abbbe4
Latte-mamu <e12m4y3k@gmail.com>
2024-09-11 23:53:19 +0900

    bmi.jsを修正

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,12 +1,9 @@
-// 変数の宣言
-let bodyWeight;
+// 変数の宣言・値の代入(体重)
+let bodyWeight = 68;
 
-// 値の代入
-bodyWeight = 68;
+// 変数の宣言・値の代入(身長)
+let height = 1.7;
 
-// 変数の宣言
-let heiGht = 1.7;
-
-// コンソールへの出力
-console.log([68] / [1.7 * 1.7]);
+// 四則演算
+console.log(bodyWeight / (height * height));
 
