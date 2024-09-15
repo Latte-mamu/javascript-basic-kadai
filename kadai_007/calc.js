ede3622ae5fad34e2be7fba4241db74594d0e10d
Latte-mamu <e12m4y3k@gmail.com>
2024-09-15 18:27:01 +0900

    kadai_007を再提出

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,21 +1,25 @@
 // 変数numに0～15までのランダムな整数を代入する
-num = Math.floor(Math.random() * 16);
+let num = Math.floor(Math.random() * 16)
 
 // 変数numの値を出力する（確認用）
 console.log(num);
 
-// 変数numの値によって、出力する文字列を切り替える
-switch (num) {
-	case 6:
-		console.log('3の倍数です');
-		break;
-	case 10:
-		console.log('5の倍数です');
-		break;
-	case 15:
-		console.log('3と5の倍数です');
-		break;
-	default:
-		console.log(num);
-		break;
+// 変数(num)が3と5の倍数の場合、「“3と5の倍数です”」のみが出力する
+if (num === 15) {
+	console.log('3と5の倍数です');
+}
+
+// 変数(num)が5の倍数の場合、“5の倍数です”と出力する
+else if (num === 5 || num === 10 || num === 15) {
+	console.log('5の倍数です');
+}
+
+/// 変数(num)が3の倍数の場合、“3の倍数です”と出力する
+else if (num === 3 || num === 6 || num === 9 || num === 12 || num === 15) {
+	console.log('3の倍数です');
+} 
+
+// 上記2～4のいずれでもない場合、変数(num)を出力する
+else {
+	console.log(num);
 }
