2cdbb3831139f8eaba84cdcc48b516f3b6ff853a
Latte-mamu <e12m4y3k@gmail.com>
2024-09-16 21:14:56 +0900

    kadai_007を再々提出

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,21 +1,21 @@
-// 変数numに0～15までのランダムな整数を代入する
-let num = Math.floor(Math.random() * 16)
+// 変数numに整数を代入する
+let num = 105;
 
 // 変数numの値を出力する（確認用）
 console.log(num);
 
 // 変数(num)が3と5の倍数の場合、「“3と5の倍数です”」のみが出力する
-if (num === 15) {
+if (num % 15 === 0) {
 	console.log('3と5の倍数です');
 }
 
 // 変数(num)が5の倍数の場合、“5の倍数です”と出力する
-else if (num === 5 || num === 10 || num === 15) {
+else if (num % 5 === 0) {
 	console.log('5の倍数です');
 }
 
-/// 変数(num)が3の倍数の場合、“3の倍数です”と出力する
-else if (num === 3 || num === 6 || num === 9 || num === 12 || num === 15) {
+// 変数(num)が3の倍数の場合、“3の倍数です”と出力する
+else if (num % 3 === 0) {
 	console.log('3の倍数です');
 } 
 
