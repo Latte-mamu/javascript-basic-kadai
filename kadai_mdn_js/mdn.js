78428d06346cbdc4a37fd565e3901714444b4558
Latte-mamu <e12m4y3k@gmail.com>
2024-09-23 23:52:29 +0900

    Update mdn.jsを修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -6,8 +6,8 @@ const date = new Date();
 
 const [month, day, year] = [
  	date.getMonth(),
-  	date.getDay(),
+  	date.getDate(),
   	date.getFullYear(),
 ];
 
-console.log(year + '年', month + 1 + '月', day + 22 + '日');
+console.log(year + '年', month + 1 + '月', day + '日');
